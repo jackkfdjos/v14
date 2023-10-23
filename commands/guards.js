983021a1cfcb15a0632cc21bc060777b18e9d270
jackkfdjos <83785697+jackkfdjos@users.noreply.github.com>
2023-10-23 19:44:26 +0300

    Add files via upload

---
+++ commands/guards.js
@@ -0,0 +1,103 @@
+# V14 Elite Guard Botu (Elite Guard Botu) - Limited Edition
+
+Bu Discord Guard Botu, gecikmesiz ve hatasız koruma sağlayan bir bot olarak tasarlanmıştır.
+
+![Bot Resmi](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/192426e6-ccc8-4fdf-bdde-2c5a894d65fb)
+
+## Özellikler
+
+- Hızlı ve güçlü bir koruma sağlar.
+- Kolayca kurulabilir ve yapılandırılabilir.
+- Sunucunuzdaki botları otomatik olarak tanır ve güvenlik için sadece "Guard Botları" seçebilirsiniz.
+- Reklam ve küfür koruması sunucu ayarlarına otomatik kurallar ekler ve gelişmiş Regex içerir.
+- Daha fazla detay için [Guard Ayarları](#guard-ayarları-setup) ve [Guard Koruma Ayarları](#guard-koruma-ayarları-guard-panel) bölümlerine bakabilirsiniz.
+
+## Guard Ayarları (Setup)
+
+<details>
+  <summary>Ayarları Göster</summary>
+  
+  - Log Kanalı Webhook olarak ayarlanabilir. Bu, hızlı ve güvenilir bir şekilde çalışmasını sağlar.
+  
+  ![Log Kanalı Ayarı](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/77af6ac6-022b-4fff-9ebc-b8355168ea71)
+  ![Log Kanalı Ayarı](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/3cdf1c31-aaf9-4c88-abd5-b9abcafcdc57)
+  ![Log Kanalı Ayarı](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/cfb81e10-f81a-4878-b2f0-93d0fe3f5e3d)
+  
+  - Sunucunuzdaki botlar otomatik olarak tanınır ve güvenlik için sadece "Guard Botları" seçebilirsiniz.
+
+  ![Bot Listesi](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/4a095b9b-19a8-4990-af75-173809a45596)
+
+  - Reklam ve küfür koruması sunucu ayarlarına otomatik olarak kurallar ekler ve gelişmiş Regex içerir. Bu koruma özelleştirilebilir ve devre dışı bırakılabilir.
+  ![Küfür ve Reklam Koruma](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/92e3a188-95d8-467a-bcee-cd12df32b3df)
+  
+  - İşlem yapılmazsa menü 3 dakika içinde sonlanır.
+  
+  ![Menü Süresi](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/98256883-7369-4da0-8e38-4628d2fb60d5)
+</details>
+
+## Guard Koruma Ayarları (Guard Panel)
+
+<details>
+  <summary>Koruma Ayarları Göster</summary>
+  
+  - Tek bir komut olan "Guard Menü" ile her türlü ayarı yapabilirsiniz.
+
+  ![Guard Menü](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/5292df9b-e247-45f7-bd92-060091d83b59)
+
+  - Kullanıcı veya role izin verilmiş modüller ekleyin. Limitler 1 saat boyunca geçerlidir.
+  - Rol eklemek isterseniz "Role Ekle" düğmesine tıklayarak bir rol seçebilirsiniz.
+
+
+  ![image](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/d7b454a2-6d7e-44b9-a189-f1487704b23d)
+
+  
+
+  ![Role Ekle](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/7ded2a07-d9a0-47ab-b03d-3e4cfceca12e)
+
+  ![image](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/0a80c377-4427-4e8d-8bed-ad6f2d517ad2)
+
+  ![image](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/d61d5327-7af2-4a75-9adc-d48e927c20da)
+
+  ![image](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/acaed859-5178-4be0-844c-a847b26a82a3)
+
+  ![image](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/7d1ecf06-b3cc-4ea7-82f0-80c43a731792)
+
+
+</details>
+
+## WhiteList Ayarları (Guard Panel)
+
+<details>
+  <summary>WhiteList Ayarları Göster</summary>
+  
+  - Tek bir komut olan "Guard Menü" ile her türlü ayarı yapabilirsiniz. Beyaz listedeki kullanıcıları veya rolleri görüntüleyebilir ve kaldırabilirsiniz.
+
+  ![Beyaz Liste](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/21aecfe0-afff-4322-ba9f-ad9394075935)
+
+  ![Beyaz Liste](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/8e197b89-3668-40d9-b236-db1257a10f4f)
+
+  ![Beyaz Liste](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/4767726c-182e-46e1-81e8-1b695c2bf794)
+</details>
+
+## Gelişmiş Log Sistemi (Log Kanalı)
+
+<details>
+  
+  ![Log Kanalı](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/8897b15f-f8ba-4a8a-97a7-c026b2fb8159)
+  
+  ![Log Kanalı](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/5e5bab38-b428-4a26-bb50-2b0713fb59eb)
+
+  ![Log Kanalı](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/c20c92ae-8add-46bb-9e61-c9200b606eed)
+</details>
+
+## Guard Modülleri
+
+![Modül Listesi](https://github.com/mysteriouss3/V14-Elite-Guard-Botu/assets/142053394/03f214f6-c815-4132-9326-c4b4cdf5c218)
+
+Botun ayar dosyası yok, her şey kurulumlu ve modülerdir. Sadece tokeninizi girip botu çalıştırmanız yeterlidir.
+
+## Satın Almak İçin
+
+- mysterious3 kullanıcısına ulaşın.
+
+<h1>Guard Botumuz 3 Adettir Her Module Hem Limit Hem Limit Aşıldıktan Sonra Ne Yapılacağı Size Menüler İle Sorulmaktadır Tamamen Modüler Bir Altyapıya Sahiptir.</h1>
